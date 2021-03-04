0c98517a613a420005fd6db97e9c64a05dbd7f79
Eriq Mendoza <mendozaeriqjohn@yahoo.com>
2021-03-05 07:30:18 +0800

    Deploy

---
+++ js/chunk-2d0c09b8.3d8f63a6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c09b8"],{4303:function(t,e,a){"use strict";a.r(e);var l=function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("v-card",{attrs:{flat:t.isFlat}},[a("v-card-title",[t._t("card-title")],2),a("v-card-text",{staticClass:"black--text"},[t._t("default")],2)],1)},n=[],r={props:{isFlat:{required:!0,type:Boolean,default:function(){return!1}}}},c=r,s=a("2877"),u=Object(s["a"])(c,l,n,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0c09b8.3d8f63a6.js.map
---
+++ js/chunk-2d0e5715.10595c7a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5715"],{9503:function(t,e,n){"use strict";n.r(e);var l=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("v-row",[n("v-col",{attrs:{md:"10","offset-md":"1"}},[n("h1",{staticClass:"text-center"},[t._t("title")],2),t._t("default")],2)],1)},s=[],c=n("2877"),a={},r=Object(c["a"])(a,l,s,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0e5715.10595c7a.js.map
